55ee4b485e0830b46849dbe8fa15b3da12221e02
Frances Andrews <arp227@txstate.edu>
2024-10-03 01:55:19 -0500

    Integrated SQLAlchemy models

---
+++ intro_to_flask/models.py
@@ -0,0 +1,17 @@
+from intro_to_flask import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    greeting = db.Column(db.String(256))
+
+    journal_entries = db.relationship('JournalEntry', backref='author', lazy=True)
+
+class JournalEntry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
