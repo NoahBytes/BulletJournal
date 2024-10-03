68b7d3e25ca9a4a267ade734b91784eb4d90fbc2
Athene Eule Yowell <ccyowell@icloud.com>
2024-10-03 17:43:16 -0500

    GRO-123 - Added error handling in the case of no connection with the API
    Updated back_img_route.py to handle the case where the api fails to make a connection. In that case, pass in a hard coded image.

--- intro_to_flask/background_image_python/back_img_route.py
+++ intro_to_flask/background_image_python/back_img_route.py
@@ -33,24 +33,14 @@ back_img_blueprint = Blueprint('back_img', __name__)
 @back_img_blueprint.route('/back_img',methods=['GET', 'POST'])
 @app.route('/back_img',methods=['GET'])
 def back_img(query_var):  
-  if query_var == "":
-    query_var = timeQuery()
-  py_un = PyUnsplash(api_key=app_key)
-  search = py_un.search(type_='photos', page = 1, per_page = 1, query=query_var)
-  for photo in search.entries:
-    photo.refresh()
-    returnPhoto = photo.link_download
-    print(photo.id, photo.link_download)
-  # photo = py_un.photos(type_="single", photo_id='l0_kVknpO2g')
-  return returnPhoto
-  #print(photo.entries.get_attribution(format='html'))
-  #return link.link_download
-        # print(f"{response.status_code}")
-        # if response.status_code == 200:
-        #   data = response.json()
-        #   # Extract the image URL (you can choose different sizes if needed)
-        #   image_url = data['urls']['regular']  # Use 'full', 'small', etc. as needed
-        #   return render_template('home.html', image_url=image_url)
-        # else:
-        #   return render_template('home.html', image_url=None)
+  try:
+    if query_var == "":
+      query_var = timeQuery()
+    py_un = PyUnsplash(api_key=app_key)
+    search = py_un.search(type_='photos', page = 1, per_page = 1, query=query_var)
+    for photo in search.entries:
+      photo.refresh()
+      return photo.link_download
+  except Exception as e:
+     return "static/img/alex-dukhanov-ZxZQk7777R4-unsplash.jpg"
         
