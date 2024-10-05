a90f7055971d843e0dd3b13123a46a4f4dd84b0c
Frances Andrews <arp227@txstate.edu>
2024-10-04 23:21:04 -0500

    debugging. working on implementing error handling.

--- intro_to_flask/__init__.py
+++ intro_to_flask/__init__.py
@@ -1,12 +1,25 @@
-import sys 
-sys.dont_write_bytecode = True
+import sys
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+sys.dont_write_bytecode = True
 
 app = Flask(__name__)
 
 app.secret_key = 'development key'
 app.config['SECRET_KEY']='LongAndRandomSecretKey'
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 #mail.init_app(app)
+
+# To avoid circular import issues
 from .routes import mail
+
+# Database schema
+from . import models
