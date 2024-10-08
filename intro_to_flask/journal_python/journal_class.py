2bc3744bb2ff0d18fbd0dad1a9a3d8f32c5a53b0
NoahBytes <rqh12@txstate.edu>
2024-10-08 13:18:46 -0500

    GRO-91 Created journal class to handle journal creation
    Journal class has several methods to support loading of textfiles, saving text, and deleting files.

--- intro_to_flask/journal_python/journal_class.py
+++ intro_to_flask/journal_python/journal_class.py
@@ -12,16 +12,16 @@ class Journal:
         return f"{title.replace(' ', '_')}.md"
 
     def save(self, directory="journals"):
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        
-        #Save the content to a markdown file in the directory
-        with open(os.path.join(directory, self.filename), 'w') as file:
+        journal_path = os.path.join('intro_to_flask', directory)
+
+        if not os.path.exists(journal_path):
+            os.makedirs(journal_path)
+        with open(os.path.join(journal_path, self.filename), 'w') as file:
             file.write(self.content)
         
     def load(self, directory="journals"):
         try:
-            with open(os.path.join(directory, self.filename), 'r') as file:
+            with open(os.path.join('intro_to_flask', directory, self.filename), 'r') as file:
                 self.content = file.read()
         except FileNotFoundError:
             self.content = None
@@ -29,6 +29,6 @@ class Journal:
 
     def delete(self, directory="journals"):
         try:
-            os.remove(os.path.join(directory, self.filename))
+            os.remove(os.path.join('intro_to_flask', directory, self.filename))
         except FileNotFoundError:
             raise Exception("Journal file not found")
